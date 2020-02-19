83976906b5ef6fe37c9208498ead3c96a2d888c0
Tyler Lai <lai.t.tyler@gmail.com>
2020-02-19 00:22:38 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -50,9 +50,15 @@ def movies_with_director_key(name, movies_collection)
   # :director_name key. This addition can be done by using the provided
   # movie_with_director_name method
   
+  i = 0
+  while i < movies_collection.length do
+    
+    result << movies_collection[i]
+    
+    i += 1 
+  end
   
-  
-  result = []
+  result
   
 end
 
