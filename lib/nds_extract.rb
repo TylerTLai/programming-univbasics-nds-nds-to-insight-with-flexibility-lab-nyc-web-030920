fd57d9e582d5dce128ec83e01f15b568e53c36b2
Tyler Lai <lai.t.tyler@gmail.com>
2020-02-19 00:43:36 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -94,6 +94,16 @@ def movies_with_directors_set(source)
   #
   # Array of Arrays containing all of a director's movies. Each movie will need
   # to have a :director_name key added to it.
+  
+  result = []
+  i = 0 
+  
+  while i < source.length do 
+    
+    result <<  
+    
+    i += 1 
+  end 
 end
 
 # ----------------    End of Your Code Region --------------------
